45d05a30adbc2d73e4a4ea84b521e36d700ace62
Sulistiyanto <sulistiyanto404@gmail.com>
2016-12-02 08:10:11 +0700

    update script

--- app/src/main/java/id/sulistiyanto/glideexample/MainActivity.java
+++ app/src/main/java/id/sulistiyanto/glideexample/MainActivity.java
@@ -2,12 +2,23 @@ package id.sulistiyanto.glideexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.imageView) ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
     }
 }
